7f8b1974e2efe604f9497da718461848b673746e
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2024-11-22 15:50:59 +0400

    matproxy

---
+++ lua/gpm/std/matproxy.lua
@@ -0,0 +1,11 @@
+local _G = _G
+
+---@class gpm.std.matproxy
+local matproxy = {}
+
+
+-- TODO: https://github.com/Facepunch/garrysmod/blob/master/garrysmod/lua/includes/modules/matproxy.lua#L20-L45
+
+
+
+return matproxy
