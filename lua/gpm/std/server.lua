3ca56d1c231f1915bb524c11eaf9dc1f3fe3bfeb
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2024-11-24 04:47:22 +0400

    `server` lib update

--- lua/gpm/std/server.lua
+++ lua/gpm/std/server.lua
@@ -1,3 +1,23 @@
--- TODO: https://wiki.facepunch.com/gmod/serverlist (std.server)
+local _G = _G
+local std = _G.gpm.std
 
--- TODO: https://wiki.facepunch.com/gmod/permissions (std.server.permissions)
+---@class gpm.std.server
+local server = {}
+
+if std.SERVER then
+    server.log = _G.ServerLog
+end
+
+--[[
+
+    https://wiki.facepunch.com/gmod/serverlist (std.server)
+    https://wiki.facepunch.com/gmod/Global.CanAddServerToFavorites
+    https://wiki.facepunch.com/gmod/Global.IsServerBlacklisted
+
+    https://wiki.facepunch.com/gmod/permissions (std.server.permissions)
+
+    https://wiki.facepunch.com/gmod/engine.ServerFrameTime
+
+]]
+
+return server
