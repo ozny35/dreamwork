14959f1e38bf9d4453971cf88423eab7e4dcfac7
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2024-11-24 04:53:28 +0400

    `gamemode` lib

--- lua/gpm/std/gamemode.lua
+++ lua/gpm/std/gamemode.lua
@@ -1,7 +1,16 @@
+local _G = _G
+local glua_engine = _G.engine
+
+---@class gpm.std.gamemode
+local gamemode = {
+    getName = glua_engine.ActiveGamemode,
+    getAll = glua_engine.GetGamemodes
+}
+
 --[[
 
-https://wiki.facepunch.com/gmod/engine.ActiveGamemode
-
-https://wiki.facepunch.com/gmod/engine.GetGamemodes
+https://wiki.facepunch.com/gmod/Global.DeriveGamemode
 
 ]]
+
+return gamemode
