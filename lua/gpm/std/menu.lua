b1075e77e3c5621c43c3b6a2a3d6757940a252e8
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2024-11-24 04:53:46 +0400

    Added `menu` lib

--- lua/gpm/std/menu.lua
+++ lua/gpm/std/menu.lua
@@ -1,6 +1,27 @@
--- TODO:
-    -- https://wiki.facepunch.com/gmod/gui.OpenURL
-    -- https://wiki.facepunch.com/gmod/Global.CancelLoading
+local _G = _G
+local std = _G.gpm.std
 
+---@class gpm.std.menu
+local menu = {
+    openURL = _G.gui.OpenURL
+}
 
-    -- https://wiki.facepunch.com/gmod/Global.CanAddServerToFavorites
+if std.MENU then
+
+    menu.getWorkshopStatus = _G.GetAddonStatus
+    menu.runCommand = _G.RunGameUICommand
+
+    ---@class gpm.std.menu.loading
+    local loading = {
+        getDefaultURL = _G.GetDefaultLoadingHTML,
+        getFileCount = _G.NumDownloadables,
+        getFiles = _G.GetDownloadables,
+        getStatus = _G.GetLoadStatus,
+        cancel = _G.CancelLoading,
+    }
+
+    menu.loading = loading
+
+end
+
+return menu
