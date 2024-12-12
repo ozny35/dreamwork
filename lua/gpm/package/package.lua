92491fca867d18d2fe56c240a7456b5f6b6bf9de
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2024-12-12 18:13:41 +0400

    `Package` class

--- lua/gpm/package/package.lua
+++ lua/gpm/package/package.lua
@@ -1,2 +1,42 @@
--- TODO: add more files in this folder
--- TODO: create a package class
+local _G = _G
+
+---@class gpm
+local gpm = _G.gpm
+
+local std = gpm.std
+local class = std.class
+local Version = std.Version
+
+---@alias Package gpm.Package
+---@class gpm.Package: gpm.std.Object
+---@field __class gpm.PackageClass
+---@field name string
+---@field version Version
+---@field private cvar_callbacks table
+local Package = class.base( "Package" )
+
+local cache = {}
+
+---@param name string
+---@param version string | Version
+---@protected
+function Package:__init( name, version )
+    local prefix = name .. "@" .. version
+    self.version = Version( version )
+    cache[ prefix ] = self
+
+    package.cvar_callbacks = {}
+
+end
+
+---@protected
+function Package.__new( name, version )
+    return cache[ name .. "@" .. version ]
+end
+
+---@class gpm.PackageClass: gpm.Package
+---@field __base gpm.Package
+---@overload fun( name: string, version: string | Version ): Package
+local PackageClass = class.create( Package )
+gpm.Package = PackageClass
+
