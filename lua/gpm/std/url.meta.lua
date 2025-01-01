151ab465edf55bf9b3068bcd648ff2ec4b0b6e5d
Retro <44505837+dankmolot@users.noreply.github.com>
2025-01-01 19:01:00 +0200

    [WIP] url meta

---
+++ lua/gpm/std/url.meta.lua
@@ -0,0 +1,13 @@
+---@meta
+
+---@alias URLState gpm.std.URLState
+---@class gpm.std.URLState
+---@field scheme string?
+---@field username string?
+---@field password string?
+---@field hostname string | table | number | nil
+---@field port number?
+---@field path string | table | nil
+---@field query string | URLSearchParams
+
+
