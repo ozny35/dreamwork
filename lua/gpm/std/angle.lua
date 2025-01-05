f9bf4d0b988d69b94341192a0f597b4566710bbf
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2025-01-05 09:44:09 +0400

    Early angle class

--- lua/gpm/std/angle.lua
+++ lua/gpm/std/angle.lua
@@ -1,10 +1,9 @@
 local _G = _G
-local glua_Angle = _G.Angle
 
 local gpm = _G.gpm
 local std = gpm.std
-local debug = std.debug
-local debug_setmetatable = debug.setmetatable
+local debug, math = std.debug, std.math
+local setmetatable = std.setmetatable
 
 local ANGLE = debug.findmetatable( "Angle" )
 if ANGLE == nil then return end
@@ -24,22 +23,138 @@ local Angle = std.class.base( "Angle" )
 
 do
 
-    local ANGLE_Unpack = ANGLE.Unpack
     local string_format = std.string.format
 
     ---@protected
     ---@private
     function Angle:__tostring()
-        return string_format( "Angle: %p [%d, %d, %d]", self, ANGLE_Unpack( self ) )
+        return string_format( "Angle: %p [%f, %f, %f]", self, self[ 1 ], self[ 2 ], self[ 3 ] )
     end
 
 end
 
 ---@protected
 function Angle.__new( pitch, yaw, roll )
-    local angle = glua_Angle( pitch, yaw, roll )
-    debug_setmetatable( angle, Angle )
-    return angle
+    return setmetatable( { pitch, yaw, roll }, Angle )
+end
+
+-- Addition
+
+---
+---@param other Angle
+---@return Angle
+function Angle:add( other )
+    self[ 1 ] = self[ 1 ] + other[ 1 ]
+    self[ 2 ] = self[ 2 ] + other[ 2 ]
+    self[ 3 ] = self[ 3 ] + other[ 3 ]
+    return self
+end
+
+---
+---@param other Angle
+---@return Angle
+function Angle:__add( other )
+    return setmetatable( {
+        self[ 1 ] + other[ 1 ],
+        self[ 2 ] + other[ 2 ],
+        self[ 3 ] + other[ 3 ]
+    }, Angle )
+end
+
+-- Subtraction
+
+---
+---@param other Angle
+---@return Angle
+function Angle:sub( other )
+    self[ 1 ] = self[ 1 ] - other[ 1 ]
+    self[ 2 ] = self[ 2 ] - other[ 2 ]
+    self[ 3 ] = self[ 3 ] - other[ 3 ]
+    return self
+end
+
+---
+---@param other Angle
+---@return Angle
+function Angle:__sub( other )
+    return setmetatable( {
+        self[ 1 ] - other[ 1 ],
+        self[ 2 ] - other[ 2 ],
+        self[ 3 ] - other[ 3 ]
+    }, Angle )
+end
+
+-- Multiplication
+function Angle:mul( other )
+    self[ 1 ] = self[ 1 ] * other[ 1 ]
+    self[ 2 ] = self[ 2 ] * other[ 2 ]
+    self[ 3 ] = self[ 3 ] * other[ 3 ]
+    return self
+end
+
+---
+---@param other Angle
+---@return Angle
+function Angle:__mul( other )
+    return setmetatable( {
+        self[ 1 ] * other[ 1 ],
+        self[ 2 ] * other[ 2 ],
+        self[ 3 ] * other[ 3 ]
+    }, Angle )
+end
+
+-- Division
+
+---
+---@param other Angle
+---@return Angle
+function Angle:div( other )
+    self[ 1 ] = self[ 1 ] / other[ 1 ]
+    self[ 2 ] = self[ 2 ] / other[ 2 ]
+    self[ 3 ] = self[ 3 ] / other[ 3 ]
+    return self
+end
+
+---
+---@param other Angle
+---@return Angle
+function Angle:__div( other )
+    return setmetatable( {
+        self[ 1 ] / other[ 1 ],
+        self[ 2 ] / other[ 2 ],
+        self[ 3 ] / other[ 3 ]
+    }, Angle )
+end
+
+-- Lerp
+
+do
+
+    local math_lerp = math.lerp
+
+    ---
+    ---@param other Angle
+    ---@param frac number
+    ---@return Angle
+    function Angle:lerp( other, frac )
+        self[ 1 ] = math_lerp( frac, self[ 1 ], other[ 1 ] )
+        self[ 2 ] = math_lerp( frac, self[ 2 ], other[ 2 ] )
+        self[ 3 ] = math_lerp( frac, self[ 3 ], other[ 3 ] )
+        return self
+    end
+
+    ---
+    ---@param other Angle
+    ---@param frac number
+    ---@return Angle
+    function Angle:__lerp( other, frac )
+        return setmetatable( {
+            math_lerp( frac, self[ 1 ], other[ 1 ] ),
+            math_lerp( frac, self[ 2 ], other[ 2 ] ),
+            math_lerp( frac, self[ 3 ], other[ 3 ] )
+        }, Angle )
+    end
+
 end
 
 ---@class gpm.std.AngleClass: gpm.std.Angle
@@ -47,6 +162,6 @@ end
 ---@overload fun(): Angle
 local AngleClass = std.class.create( Angle )
 
--- print( AngleClass( 1, 0, 0 ) )
+-- print( AngleClass( 10.11, 0, 0 ) )
 
-return AngleClass
+return Angle
