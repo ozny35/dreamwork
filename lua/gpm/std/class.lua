072cde15eeccd968b1f7458fb2ffb52caa056f9a
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2025-01-24 15:55:36 +0400

    classes `__new` now have self ( base )

--- lua/gpm/std/class.lua
+++ lua/gpm/std/class.lua
@@ -90,10 +90,8 @@ class.init = init
 local function new( base, ... )
     local fn = rawget( base, "__new" )
     if is_function( fn ) then
-        local obj = fn( base.__class, ... )
-        if obj ~= nil then
-            return obj
-        end
+        local obj = fn( base, ... )
+        if obj ~= nil then return obj end
     end
 
     return init( setmetatable( {}, base ), base, ... )
