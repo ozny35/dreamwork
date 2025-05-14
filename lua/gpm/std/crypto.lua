dc4bde29471fabf2a61e0834de8a1a9aa056a419
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2025-05-15 03:11:48 +0400

    okay, not so fast i think

--- lua/gpm/std/crypto.lua
+++ lua/gpm/std/crypto.lua
@@ -216,36 +216,43 @@ do
         return hash
     end
 
-    local crc32_lookup = {}
+    if crypto.crc32 == nil then
+        if glua_util.CRC == nil then
+            local crc32_lookup = {}
 
-    for i = 0, 255, 1 do
-        local crc = i
+            for i = 0, 255, 1 do
+                local crc = i
 
-        for _ = 1, 8 do
-            if bit_band( crc, 1 ) == 1 then
-                crc = bit_bxor( bit_rshift( crc, 1 ), 0xedb88320 )
-            else
-                crc = bit_rshift( crc, 1 )
+                for _ = 1, 8 do
+                    if bit_band( crc, 1 ) == 1 then
+                        crc = bit_bxor( bit_rshift( crc, 1 ), 0xedb88320 )
+                    else
+                        crc = bit_rshift( crc, 1 )
+                    end
+                end
+
+                crc32_lookup[ i ] = crc
             end
+
+            --- [SHARED AND MENU]
+            ---
+            --- Calculates the CRC-32 checksum of a string.
+            ---
+            ---@param str string The string to calculate the CRC-32 checksum of.
+            ---@return integer checksum The CRC-32 checksum of the string.
+            ---@diagnostic disable-next-line: duplicate-set-field
+            function crypto.crc32( str )
+                local crc = 0xFFFFFFFF
+
+                for i = 1, string_len( str ), 1 do
+                    crc = bit_bxor( bit_rshift( crc, 8 ), crc32_lookup[ bit_band( bit_bxor( crc, string_byte( str, i ) ), 0xFF ) ] )
+                end
+
+                return bit_band( bit_bnot( crc ), 0xffffffff )
+            end
+        else
+            crypto.crc32 = glua_util.CRC
         end
-
-        crc32_lookup[ i ] = crc
-    end
-
-    --- [SHARED AND MENU]
-    ---
-    --- Calculates the CRC-32 checksum of a string.
-    ---
-    ---@param str string The string to calculate the CRC-32 checksum of.
-    ---@return integer checksum The CRC-32 checksum of the string.
-    function crypto.crc32( str )
-        local crc = 0xFFFFFFFF
-
-        for i = 1, string_len( str ), 1 do
-            crc = bit_bxor( bit_rshift( crc, 8 ), crc32_lookup[ bit_band( bit_bxor( crc, string_byte( str, i ) ), 0xFF ) ] )
-        end
-
-        return bit_band( bit_bnot( crc ), 0xffffffff )
     end
 
 end
