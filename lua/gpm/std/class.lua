c496fe0c1351f29c8ada1762978b4c56c975a35a
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2025-05-18 17:57:22 +0400

    private classes!

--- lua/gpm/std/class.lua
+++ lua/gpm/std/class.lua
@@ -4,11 +4,9 @@ local std = _G.gpm.std
 local debug = std.debug
 local string = std.string
 
-local string_sub = string.sub
-local string_format = string.format
+local debug_newproxy = debug.newproxy
 local setmetatable = std.setmetatable
-local debug_getmetatable = debug.getmetatable
-local raw_get, raw_pairs = std.raw.get, std.raw.pairs
+local raw_get = std.raw.get
 
 --- [SHARED AND MENU]
 ---
@@ -17,8 +15,6 @@ local raw_get, raw_pairs = std.raw.get, std.raw.pairs
 ---@class gpm.std.class
 local class = {}
 std.class = class
-
--- TODO: re-view/work private classes metatable cloning maybe make name tables and userdata colying
 
 ---@alias Object gpm.std.Object
 ---@class gpm.std.Object
@@ -32,17 +28,27 @@ std.class = class
 ---@class gpm.std.Class : gpm.std.Object
 ---@field __base gpm.std.Object The base of the class.
 ---@field __parent gpm.std.Class | nil The parent of the class.
----@field __private boolean If the class is private (cannot be inherited).
+---@field __private boolean If the class is private.
 ---@field private __inherited fun( parent: gpm.std.Class, child: gpm.std.Class ) | nil The function that will be called when the class is inherited.
+
+---@type table<gpm.std.Object, userdata>
+local templates = {}
+
+setmetatable( templates, {
+    __mode = "k"
+} )
 
 do
 
     local debug_getmetavalue = debug.getmetavalue
+    local debug_getmetatable = debug.getmetatable
+    local raw_pairs = std.raw.pairs
+    local string_sub = string.sub
 
     ---@param obj gpm.std.Object The object to convert to a string.
     ---@return string str The string representation of the object.
     local function base__tostring( obj )
-        return string_format( "%s: %p", debug_getmetavalue( obj, "__type" ) or "unknown", obj )
+        return string.format( "%s: %p", debug_getmetavalue( obj, "__type" ) or "unknown", obj )
     end
 
     ---@type table<string, boolean>
@@ -60,12 +66,30 @@ do
     ---
     ---@param name string The name of the class.
     ---@param parent gpm.std.Class? The parent of the class.
+    ---@param private boolean? If the class is private.
     ---@return gpm.std.Object base The base of the class.
-    function class.base( name, parent )
-        local base = {
-            __type = name,
-            __tostring = base__tostring
-        }
+    function class.base( name, parent, private )
+        local base
+
+        if private then
+            local template = debug_newproxy( true )
+            base = debug_getmetatable( template )
+
+            if base == nil then
+                error( "userdata metatable is missing, lua is broken - it's over" )
+            end
+
+            templates[ base ] = template
+
+            base.__type = name
+            base.__private = true
+            base.__tostring = base__tostring
+        else
+            base = {
+                __type = name,
+                __tostring = base__tostring
+            }
+        end
 
         base.__index = base
 
@@ -92,151 +116,110 @@ do
 
 end
 
+local class__call
 do
-
-    local debug_newproxy = debug.newproxy
-
-    local class__call
-    do
-
-        --- [SHARED AND MENU]
-        ---
-        --- Calls the base initialization function, <b>if it exists</b>, and returns the given object.
-        ---
-        ---@param obj table The object to initialize.
-        ---@param base gpm.std.Object The base object, aka metatable.
-        ---@param ... any? Arguments to pass to the constructor.
-        ---@return gpm.std.Object object The initialized object.
-        local function init( obj, base, ... )
-            local init_fn = raw_get( base, "__init" )
-            if init_fn ~= nil then
-                init_fn( obj, ... )
-            end
-
-            return obj
-        end
-
-        class.init = init
-
-        ---@param self gpm.std.Class The class.
-        ---@return gpm.std.Object object The new object.
-        function class__call( self, ... )
-            local base = raw_get( self, "__base" )
-            if base == nil then
-                error( "class base is missing, class creation failed.", 2 )
-            end
-
-            ---@cast base gpm.std.Object
-
-            local obj
-
-            if raw_get( self, "__private" ) then
-
-                local template = raw_get( self, "__template" )
-                if template == nil then
-                    error( "template is missing, class creation failed.", 2 )
-                end
-
-                obj = debug_newproxy( template )
-
-            else
-
-                local new_fn = raw_get( base, "__new" )
-                if new_fn ~= nil then
-                    obj = new_fn( base, ... )
-                end
-
-                if obj == nil then
-                    obj = {}
-                    setmetatable( obj, base )
-                end
-
-            end
-
-            ---@cast obj gpm.std.Object
-
-            local init_fn = raw_get( base, "__init" )
-            if init_fn ~= nil then
-                init_fn( obj, ... )
-            end
-
-            return obj
-        end
-
-    end
-
-    ---@param cls gpm.std.Class The class.
-    ---@return string str The string representation of the class.
-    local function class__tostring( cls )
-        return string_format( "%sClass: %p", raw_get( raw_get( cls, "__base" ), "__type" ), cls )
-    end
-
-    local raw_set = std.raw.set
 
     --- [SHARED AND MENU]
     ---
-    --- Creates a new class from the given base.
+    --- This function is optional and can be used to re-initialize the object.
+    ---
+    --- Calls the base initialization function, <b>if it exists</b>, and returns the given object.
     ---
     ---@param base gpm.std.Object The base object, aka metatable.
-    ---@param private? boolean If the class should be private.
-    ---@return gpm.std.Class | unknown cls The class.
-    function class.create( base, private )
-        local cls = {
-            __base = base
-        }
-
-        local parent_base = raw_get( base, "__parent" )
-        if parent_base ~= nil then
-            ---@cast parent_base gpm.std.Object
-            cls.__parent = parent_base.__class
-
-            local parent = raw_get( parent_base, "__class" )
-            if parent == nil then
-                error( "parent class has no class", 2 )
-            else
-                local inherited_fn = raw_get( parent, "__inherited" )
-                if inherited_fn ~= nil then
-                    inherited_fn( parent, cls )
-                end
-            end
+    ---@param obj any The object to initialize.
+    ---@param ... any? Arguments to pass to the constructor.
+    ---@return gpm.std.Object object The initialized object.
+    function class.init( base, obj, ... )
+        local init_fn = raw_get( base, "__init" )
+        if init_fn ~= nil then
+            init_fn( obj, ... )
         end
 
-        if private then
-            cls.__private = true
-
-            local template = debug_newproxy( true )
-            cls.__template = template
-
-            local metatable = debug_getmetatable( template )
-            if metatable == nil then
-                error( "userdata metatable is missing, lol how", 2 )
-            end
-
-            setmetatable( metatable, { __index = base } )
-
-            for key, value in raw_pairs( base ) do
-                if string_sub( key, 1, 2 ) == "__" and not ( key == "__index" and value == base ) then
-                    metatable[ key ] = value
-                end
-            end
-
-            cls.__base = metatable
-            base = metatable
-        else
-            cls.__private = false
-        end
-
-        setmetatable( cls, {
-            __index = base,
-            __call = class__call,
-            __tostring = class__tostring,
-            __type = raw_get( base, "__type" ) .. "Class"
-        } )
-
-        ---@cast cls gpm.std.Object
-
-        raw_set( base, "__class", cls )
-        return cls
+        return obj
     end
 
+    ---@param self gpm.std.Class The class.
+    ---@return gpm.std.Object object The new object.
+    function class__call( self, ... )
+        local base = raw_get( self, "__base" )
+        if base == nil then
+            error( "class base is missing, class creation failed.", 2 )
+        end
+
+        ---@cast base gpm.std.Object
+
+        local obj
+
+        local new_fn = raw_get( base, "__new" )
+        if new_fn ~= nil then
+            obj = new_fn( base, ... )
+        end
+
+        if obj == nil then
+            if raw_get( base, "__private" ) then
+                obj = debug_newproxy( templates[ base ] )
+            else
+                obj = {}
+                setmetatable( obj, base )
+            end
+        end
+
+        ---@cast obj gpm.std.Object
+
+        local init_fn = raw_get( base, "__init" )
+        if init_fn ~= nil then
+            init_fn( obj, ... )
+        end
+
+        return obj
+    end
+
+end
+
+---@param cls gpm.std.Class The class.
+---@return string str The string representation of the class.
+local function class__tostring( cls )
+    return string.format( "%sClass: %p", raw_get( raw_get( cls, "__base" ), "__type" ), cls )
+end
+
+local raw_set = std.raw.set
+
+--- [SHARED AND MENU]
+---
+--- Creates a new class from the given base.
+---
+---@param base gpm.std.Object The base object, aka metatable.
+---@return gpm.std.Class | unknown cls The class.
+function class.create( base )
+    local cls = {
+        __base = base
+    }
+
+    local parent_base = raw_get( base, "__parent" )
+    if parent_base ~= nil then
+        ---@cast parent_base gpm.std.Object
+        cls.__parent = parent_base.__class
+
+        local parent = raw_get( parent_base, "__class" )
+        if parent == nil then
+            error( "parent class has no class", 2 )
+        else
+            local inherited_fn = raw_get( parent, "__inherited" )
+            if inherited_fn ~= nil then
+                inherited_fn( parent, cls )
+            end
+        end
+    end
+
+    setmetatable( cls, {
+        __index = base,
+        __call = class__call,
+        __tostring = class__tostring,
+        __type = raw_get( base, "__type" ) .. "Class"
+    } )
+
+    ---@cast cls gpm.std.Object
+
+    raw_set( base, "__class", cls )
+    return cls
 end
