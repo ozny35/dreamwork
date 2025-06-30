96826347ce77a6810a81a7fc251508a9e297d6b0
unknown-gd <44779902+unknown-gd@users.noreply.github.com>
2025-07-01 01:00:40 +0400

    Meet the trully `crc32` and more!

--- lua/gpm/std/crypto.crc.lua
+++ lua/gpm/std/crypto.crc.lua
@@ -13,6 +13,8 @@ local string = std.string
 local string_len = string.len
 local string_byte = string.byte
 
+local setmetatable = std.setmetatable
+
 -- Tests: https://www.texttool.com/crc-online
 
 do
@@ -20,14 +22,14 @@ do
     ---@type table<integer, table<integer, integer>>
     local crc8_lookup = {}
 
-    std.setmetatable( crc8_lookup, {
+    setmetatable( crc8_lookup, {
         __index = function( self, poly )
             local hash_map = {}
 
             for uint8 = 0, 255, 1 do
                 local crc = uint8
 
-                for _ = 1, 8 do
+                for _ = 1, 8, 1 do
                     if bit_band( crc, 0x80 ) == 0 then
                         crc = bit_lshift( crc, 1 )
                     else
@@ -100,7 +102,7 @@ do
 
     --- [SHARED AND MENU]
     ---
-    --- Calculates the CRC-8 checksum of the specified string.
+    --- Calculates the CRC-8 checksum of the specified string using the Maxim algorithm.
     ---
     --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-8 checksum.
     ---
@@ -112,7 +114,7 @@ do
 
     --- [SHARED AND MENU]
     ---
-    --- Calculates the CRC-8 checksum of the specified string.
+    --- Calculates the CRC-8 checksum of the specified string using the ROHC algorithm.
     ---
     --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-8 checksum.
     ---
@@ -124,7 +126,7 @@ do
 
     --- [SHARED AND MENU]
     ---
-    --- Calculates the CRC-8 checksum of the specified string.
+    --- Calculates the CRC-8 checksum of the specified string using the CDMA2000 algorithm.
     ---
     --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-8 checksum.
     ---
@@ -141,24 +143,26 @@ do
     ---@type table<integer, table<integer, integer>>
     local crc16_lookup = {}
 
-    std.setmetatable( crc16_lookup, {
+    setmetatable( crc16_lookup, {
         __index = function( self, uint17 )
-            local ref_out = bit_band( uint17, 0x20000 ) == 0x20000
-            local ref_in = bit_band( uint17, 0x10000 ) == 0x10000
+            local ref_out = bit_band( uint17, 0x20000 ) ~= 0
+            local ref_in = bit_band( uint17, 0x10000 ) ~= 0
             local poly = bit_band( uint17, 0xFFFF )
 
             local hash_map = {}
 
             for uint8 = 0, 255, 1 do
-                local crc = uint8
+                local crc
 
                 if ref_in then
-                    crc = bit_reverse( crc, 8 )
+                    crc = bit_reverse( uint8, 8 )
+                else
+                    crc = uint8
                 end
 
                 crc = bit_lshift( crc, 8 )
 
-                for _ = 1, 8 do
+                for _ = 1, 8, 1 do
                     if bit_band( crc, 0x8000 ) == 0 then
                         crc = bit_lshift( crc, 1 )
                     else
@@ -188,6 +192,11 @@ do
     --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-16 checksum.
     ---
     ---@param raw_str string The string used to calculate the CRC-16 checksum.
+    ---@param poly? integer The polynomial used to calculate the CRC-16 checksum.
+    ---@param init? integer The initial value used to calculate the CRC-16 checksum.
+    ---@param ref_in? boolean Whether to reflect the input data before processing.
+    ---@param ref_out? boolean Whether to reflect the output data after processing.
+    ---@param xor_out? integer The XOR value used to calculate the CRC-16 checksum.
     ---@return integer checksum The CRC-16 checksum, which is greater or equal to 0, and less than 2^16 (0x10000).
     local function crypto_crc16( raw_str, poly, init, ref_in, ref_out, xor_out )
         ref_in = ref_in ~= false
@@ -238,7 +247,7 @@ do
 
     --- [SHARED AND MENU]
     ---
-    --- Calculates the CRC-16 checksum of the specified string.
+    --- Calculates the CRC-16 checksum of the specified string using the Maxim algorithm.
     ---
     --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-16 checksum.
     ---
@@ -250,7 +259,7 @@ do
 
     --- [SHARED AND MENU]
     ---
-    --- Calculates the CRC-16 checksum of the specified string.
+    --- Calculates the CRC-16 checksum of the specified string using the XModem algorithm.
     ---
     --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-16 checksum.
     ---
@@ -262,7 +271,7 @@ do
 
     --- [SHARED AND MENU]
     ---
-    --- Calculates the CRC-16 checksum of the specified string.
+    --- Calculates the CRC-16 checksum of the specified string using the USB algorithm.
     ---
     --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-16 checksum.
     ---
@@ -274,53 +283,191 @@ do
 
 end
 
--- TODO: update crc32 to use the same args as crc8/16
+do
 
-local crc32 = ( _G.util or {} ).CRC
-if crc32 == nil then
+    local raw_tonumber = std.raw.tonumber
+    local crc32 = ( _G.util or {} ).CRC
 
-    local bit_bnot = bit.bnot
-
-    ---@type table<integer, integer>
+    ---@type table<integer, table<integer, integer>>
     local crc32_lookup = {}
 
-    for i = 0, 255, 1 do
-        local crc = i
+    setmetatable( crc32_lookup, {
+        __index = function( self, poly )
+            local hash_map = {}
 
-        for _ = 1, 8 do
-            if bit_band( crc, 1 ) == 1 then
-                crc = bit_bxor( bit_rshift( crc, 1 ), 0xedb88320 )
+            for uint8 = 0, 255, 1 do
+                local crc = bit_lshift( uint8, 24 )
+
+                for _ = 1, 8, 1 do
+                    if bit_band( crc, 0x80000000 ) == 0 then
+                        crc = bit_lshift( crc, 1 )
+                    else
+                        crc = bit_bxor( bit_lshift( crc, 1 ), poly )
+                    end
+                end
+
+                hash_map[ uint8 ] = crc % 0x100000000
+            end
+
+            self[ poly ] = hash_map
+
+            return hash_map
+        end
+    } )
+
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@param poly? integer The polynomial used to calculate the CRC-32 checksum.
+    ---@param init? integer The initial value used to calculate the CRC-32 checksum.
+    ---@param ref_in? boolean Whether to reflect the input data before processing.
+    ---@param ref_out? boolean Whether to reflect the output data after processing.
+    ---@param xor_out? integer The XOR value used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    local function crypto_crc32( raw_str, poly, init, ref_in, ref_out, xor_out )
+        ref_in = ref_in ~= false
+        ref_out = ref_out ~= false
+
+        if init == nil then
+            init = 0xFFFFFFFF
+        else
+            init = init % 0x100000000
+        end
+
+        if poly == nil then
+            poly = 0x04C11DB7
+        else
+            poly = poly % 0x100000000
+        end
+
+        if xor_out == nil then
+            xor_out = 0xFFFFFFFF
+        else
+            xor_out = xor_out % 0x100000000
+        end
+
+        if poly == 0x04C11DB7 and init == 0xFFFFFFFF and ref_in and ref_out and xor_out == 0xFFFFFFFF then
+            return raw_tonumber( crc32( raw_str ) or 0, 10 ) or 0
+        end
+
+        local hash_map = crc32_lookup[ poly ]
+        local crc = init
+
+        for index = 1, string_len( raw_str ), 1 do
+            if ref_in then
+                crc = bit_bxor( bit_lshift( crc, 8 ), hash_map[ bit_band( bit_bxor( bit_band( bit_rshift( crc, 24 ), 0xFF ), bit_reverse( string_byte( raw_str, index, index ), 8 ) ), 0xFF ) ] )
             else
-                crc = bit_rshift( crc, 1 )
+                crc = bit_bxor( bit_lshift( crc, 8 ), hash_map[ bit_band( bit_bxor( bit_band( bit_rshift( crc, 24 ), 0xFF ), string_byte( raw_str, index, index ) ), 0xFF ) ] )
             end
         end
 
-        crc32_lookup[ i ] = crc
+        if ref_out then
+            crc = bit_reverse( crc, 32 )
+        end
+
+        return bit_bxor( crc, xor_out ) % 0x100000000
+    end
+
+    crypto.crc32 = crypto_crc32
+
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string using the BZip2 algorithm.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32bzip2( raw_str )
+        return crypto_crc32( raw_str, 0x04C11DB7, 0xFFFFFFFF, false, false, 0xFFFFFFFF )
     end
 
     --- [SHARED AND MENU]
     ---
-    --- Calculates the CRC-32 checksum of a string.
+    --- Calculates the CRC-32 checksum of the specified string using the Castagnoli algorithm.
     ---
-    ---@param str string The string to calculate the CRC-32 checksum of.
-    ---@return integer checksum The CRC-32 checksum of the string.
-    ---@diagnostic disable-next-line: duplicate-set-field
-    function crypto.crc32( str )
-        local crc = 0xFFFFFFFF
-
-        for index = 1, string_len( str ), 1 do
-            crc = bit_bxor( bit_rshift( crc, 8 ), crc32_lookup[ bit_band( bit_bxor( crc, string_byte( str, index, index ) ), 0xFF ) ] )
-        end
-
-        return bit_bnot( crc ) % 0x100000000
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32c( raw_str )
+        return crypto_crc32( raw_str, 0x1EDC6F41, 0xFFFFFFFF, true, true, 0xFFFFFFFF )
     end
 
-else
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string using the D algorithm.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32d( raw_str )
+        return crypto_crc32( raw_str, 0xA833982B, 0xFFFFFFFF, true, true, 0xFFFFFFFF )
+    end
 
-    local raw_tonumber = std.raw.tonumber
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string using the JamCRC algorithm.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32jamcrc( raw_str )
+        return crypto_crc32( raw_str, 0x04C11DB7, 0xFFFFFFFF, true, true, 0x00000000 )
+    end
 
-    function crypto.crc32( str )
-        return raw_tonumber( crc32( str ), 10 ) or 0
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string using the MPEG-2 algorithm.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32mpeg2( raw_str )
+        return crypto_crc32( raw_str, 0x04C11DB7, 0xFFFFFFFF, false, false, 0x00000000 )
+    end
+
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string using the POSIX algorithm.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32posix( raw_str )
+        return crypto_crc32( raw_str, 0x04C11DB7, 0x00000000, false, false, 0xFFFFFFFF )
+    end
+
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string using the Q algorithm.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32q( raw_str )
+        return crypto_crc32( raw_str, 0x814141AB, 0x00000000, false, false, 0x00000000 )
+    end
+
+    --- [SHARED AND MENU]
+    ---
+    --- Calculates the CRC-32 checksum of the specified string using the XFER algorithm.
+    ---
+    --- See https://en.wikipedia.org/wiki/Cyclic_redundancy_check for the definition of the CRC-32 checksum.
+    ---
+    ---@param raw_str string The string used to calculate the CRC-32 checksum.
+    ---@return integer checksum The CRC-32 checksum, which is greater or equal to 0, and less than 2^32 (0x100000000).
+    function crypto.crc32xfer( raw_str )
+        return crypto_crc32( raw_str, 0x000000AF, 0x00000000, false, false, 0x00000000 )
     end
 
 end
